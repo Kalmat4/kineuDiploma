69932f5da65458d609012dfa0642892c32463edd
Almat <59765743+Kalmat4@users.noreply.github.com>
2023-05-19 15:18:33 +0600

    create Diploma

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+require "pages/indexHead.php";
+?>
+
+<div class="template">
+
+<?php
+require "pages/header.php";
+require "pages/main.php";
+require "pages/footer.php";
+?>
---
+++ pages/pagesHead.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="icon" type="image/x-icon" href="../images/logo.ico">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <?php if(!($_SERVER['REQUEST_URI'] == '/index.php')){ ?>
+    <script src="../js/google-translate.js"></script>
+    <?php } ?>
+    <title>БИБЛИОТЕКА КОСТАНАЙСКОГО ИНЖЕНЕРНО-ЭКОНОМИЧЕСКОГО УНИВЕРСИТЕТА ИМ. М. ДУЛАТОВА</title>
+</head>
+<body class="page_fix">
